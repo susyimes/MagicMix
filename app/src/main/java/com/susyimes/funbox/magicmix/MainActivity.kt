b0b3c6854563f3c3cb40953f7422271ff79e1d25
susyimes <705710652@qq.com>
2020-11-26 16:17:24 +0800

    Initial commit

---
+++ app/src/main/java/com/susyimes/funbox/magicmix/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.susyimes.funbox.magicmix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
